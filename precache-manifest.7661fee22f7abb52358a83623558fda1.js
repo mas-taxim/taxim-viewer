65a06e14ece78d9462c593ad35cea3db526f5aba
joonas <joonas.yoon@gmail.com>
2023-03-08 19:39:14 +0900

    First build

---
+++ precache-manifest.7661fee22f7abb52358a83623558fda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1268bc9ef1f295e2eb1e",
+    "url": "./static/css/main.d0bc43ba.chunk.css"
+  },
+  {
+    "revision": "1268bc9ef1f295e2eb1e",
+    "url": "./static/js/main.f2a176dc.chunk.js"
+  },
+  {
+    "revision": "bccf0559ce981ac4a2ec",
+    "url": "./static/js/runtime~main.bcdec3bf.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "./static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "./static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "9b4e2688bbcc49b772c7010e5ab72b40",
+    "url": "./index.html"
+  }
+];
