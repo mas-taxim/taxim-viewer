d11905705ec9b8d92d4358ec968b36bca9a3c5fb
Joona Yoon <joonas.yoon@gmail.com>
2023-03-10 18:20:51 +0900

    feat: draw dynamic multiple overlays on map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,6 +1,6 @@
 /* global kakao */
 
-import React, { useEffect, useState, useMemo } from "react"
+import React, { useEffect, useState } from "react"
 import KakaoMap from "../components/KakaoMap"
 
 import Dot from "../components/Dot"
@@ -8,9 +8,17 @@ import { CustomOverlayMap } from "react-kakao-maps-sdk"
 
 type MarkerPosition = {
   key: string | null
+  color?: string
+  size: number
   lat: number
   lng: number
 }
+
+interface ColorName {
+  [key: string]: string
+}
+
+const randomColor = () => "#" + Math.random().toString(16).slice(-6)
 
 export default function Map() {
   const latitude = 37.52897
@@ -18,13 +26,74 @@ export default function Map() {
   const [markerPositions, setMarkerPositions] = useState<Array<MarkerPosition>>(
     []
   )
+  const [level, setLevel] = useState<number>(3)
 
   useEffect(() => {
-    setMarkerPositions([
-      { key: "marker-1", lat: 37.52897, lng: 126.917101 },
-      { key: "marker-2", lat: 37.5268796, lng: 126.919094 },
-      { key: "marker-3", lat: 37.527502, lng: 126.920146 },
-    ])
+    let timer: any = null
+    ;(async () => {
+      // dummy test file
+      const { logs } = await fetch(
+        "https://api.github.com/gists/43807f8eee7f54d19f167130234fdc15"
+      )
+        .then((r) => r.json())
+        .then(({ files }) => files["20230308005358.min.json"])
+        .then(({ content }) => JSON.parse(content))
+
+      if (!logs) return
+
+      let index = 0
+      const colors: ColorName = {}
+
+      timer = setInterval(() => {
+        if (index >= logs.length) {
+          return
+        }
+        const { vehicles, task } = logs[index]
+        index += 1
+
+        vehicles.forEach(({ name }: any) => {
+          if (!(name in colors)) {
+            colors[name] = randomColor()
+          }
+        })
+        task.forEach(({ id }: any) => {
+          const name = `task-${id}`
+          if (!(name in colors)) {
+            colors[name] = randomColor()
+          }
+        })
+
+        const vMarkers = vehicles.map(({ name, lat, lng }: any) => ({
+          key: name,
+          color: colors[name],
+          size: 6,
+          lat,
+          lng,
+        }))
+
+        const tMarkers = task
+          .map(({ id, pick_lat, pick_lng, drop_lat, drop_lng }: any) => [
+            {
+              key: `task-${id}-pick`,
+              color: colors[`task-${id}`],
+              size: 3,
+              lat: pick_lat,
+              lng: pick_lng,
+            },
+            {
+              key: `task-${id}-drop`,
+              color: colors[`task-${id}`],
+              size: 3,
+              lat: drop_lat,
+              lng: drop_lng,
+            },
+          ])
+          .flat()
+
+        setMarkerPositions([...vMarkers, ...tMarkers])
+      }, 33)
+    })()
+    return () => clearInterval(timer)
   }, [])
 
   return (
@@ -34,23 +103,28 @@ export default function Map() {
           lat: latitude,
           lng: longitude,
         }}
-        level={3}
+        level={level}
+        onZoomChanged={(map) => setLevel(map.getLevel())}
         style={{
           width: "100%",
           height: "100vh",
         }}
       >
-        {markerPositions.map(({ key, lat, lng }: MarkerPosition) => (
-          <CustomOverlayMap
-            key={key}
-            position={{
-              lat,
-              lng,
-            }}
-          >
-            <Dot size={2} />
-          </CustomOverlayMap>
-        ))}
+        <>
+          {markerPositions.map(
+            ({ key, color, size, lat, lng }: MarkerPosition) => (
+              <CustomOverlayMap
+                key={key}
+                position={{
+                  lat,
+                  lng,
+                }}
+              >
+                <Dot color={color || "dodgerblue"} size={size / level} />
+              </CustomOverlayMap>
+            )
+          )}
+        </>
       </KakaoMap>
     </>
   )
