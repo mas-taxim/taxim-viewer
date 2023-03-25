cc11eda06f4c33d8247777e19cc4543874fbeceb
joonas <joonas.yoon@gmail.com>
2023-03-25 17:42:12 +0900

    fix: create more darker colors

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,11 +1,34 @@
 export const randomColor = () => "#" + Math.random().toString(16).slice(-6)
 
+export const randomLightColor = () => {
+  const rgb = shuffleArray([
+    Math.random() * 128 + 48,
+    Math.random() * 128 + 80,
+    Math.random() * 64 + 192
+  ].map(Math.round));
+  return arrayToHex(rgb)
+}
+
 export const randomDarkColor = () => {
-  let rgb = [Math.random() * 255, Math.random() * 255, Math.random() * 255].map(
-    Math.round
-  )
-  if (rgb.filter((c) => c >= 128).length > 2) {
-    rgb = rgb.map((c) => Math.max(0, (c - 128) * 2))
+  const rgb = shuffleArray([
+    Math.random() * 64,
+    Math.random() * 128,
+    Math.random() * 64 + 128
+  ].map(Math.round))
+  return arrayToHex(rgb)
+}
+
+function arrayToHex(rgb: number[]) {
+  return "#" + rgb.map((c: number) => c.toString(16).padStart(2, "0")).join("")
+}
+
+function shuffleArray(array: any[]) {
+  const a = [...array]
+  for (var i = a.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = a[i];
+      a[i] = a[j];
+      a[j] = temp;
   }
-  return "#" + rgb.map((c) => c.toString(16).padStart(2, "0")).join("")
+  return a
 }
