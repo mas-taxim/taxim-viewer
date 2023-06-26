6402b755047d569e6201d8c09c14d9e6faac5e1b
joonas-yoon <joonas-yoon@users.noreply.github.com>
2023-06-26 17:01:56 +0000

    deploy: 9ed2a9484e5c7a5f011267c44e0d60b149585be6

--- precache-manifest.c1fc9fbef626d140c8235c55ca13db68.js
+++ precache-manifest.c1fc9fbef626d140c8235c55ca13db68.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "0a90cca26c45eff3ed58",
+    "revision": "d6f885eb124290a9196d",
     "url": "./static/css/main.d50d00d3.chunk.css"
   },
   {
-    "revision": "0a90cca26c45eff3ed58",
-    "url": "./static/js/main.8b9a6e76.chunk.js"
+    "revision": "d6f885eb124290a9196d",
+    "url": "./static/js/main.8bdd88a9.chunk.js"
   },
   {
     "revision": "17b7c7782400386c241c",
     "url": "./static/js/runtime~main.b60ac946.js"
   },
   {
-    "revision": "1ddca54ba2d800c81f37",
+    "revision": "42dbae38192d1984698a",
     "url": "./static/css/2.d7e1c09f.chunk.css"
   },
   {
-    "revision": "1ddca54ba2d800c81f37",
-    "url": "./static/js/2.2e1a3636.chunk.js"
+    "revision": "42dbae38192d1984698a",
+    "url": "./static/js/2.6eb61784.chunk.js"
   },
   {
     "revision": "36787e65c8dde2cdd8bf",
     "url": "./static/js/3.7cabcc74.chunk.js"
   },
   {
-    "revision": "d2b9c9c389ec382b84721dd3a38922f9",
+    "revision": "8dfda657582392b8aa8a227ad02c35fa",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.50d673e45e9d08e998685e6c0311aacd.js"
+  "./precache-manifest.c1fc9fbef626d140c8235c55ca13db68.js"
 );
 
 workbox.clientsClaim();
